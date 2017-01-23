756a43949964a0c2d44a32035eb67841c7bc6b88
Addison Benzshawel <john-benzshawel@uiowa.edu>
2017-01-22 21:41:00 -0600

    updating .gitignore

--- Quiz-O-Matic/app/models/login.model.ts
+++
@@ -1,6 +0,0 @@
-
-export class LoginModel {
-   public username: string;
-   public password: string;
-   public rememberMe: boolean;
-}
--- Quiz-O-Matic/app/models/user.model.ts
+++
@@ -1,6 +0,0 @@
-export class User {
-    username: string;
-    token: string;
-    firstName: string;
-    lastName: string;
-}
