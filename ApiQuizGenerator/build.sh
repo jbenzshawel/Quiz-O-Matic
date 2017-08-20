f09eac7371c8bdaf393443db304079037350ebfb
Addison Benzshawel <jbenzshawel@gmail.com>
2017-08-20 09:26:10 -0500

    attempting to fix CI build

--- ApiQuizGenerator/build.sh
+++ ApiQuizGenerator/build.sh
@@ -1,5 +1,5 @@
 set -ev
-dotnet restore
+dotnet restore ./ApiQuizGenerator/src/ApiQuizGenerator/ApiQuizGenerator.csproj
 dotnet build ./ApiQuizGenerator/src/ApiQuizGenerator/ApiQuizGenerator.csproj
 #dotnet test ./ApiQuizGenerator/test/ApiQuizGenerator.Tests/project.json
 
